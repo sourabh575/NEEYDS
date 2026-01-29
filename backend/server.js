1d21acf567a11449e72053ead59d8e81cbec4216
sourabh575 <139305542+sourabh575@users.noreply.github.com>
2026-01-29 14:54:45 +0530

    fix postroutes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
-import postRoutes from "./routes/PostRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+
 
 dotenv.config();
 const app = express();
