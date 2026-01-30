fb20aac2e4e5a9bafdf9575151cf222e2159742b
sourabh575 <139305542+sourabh575@users.noreply.github.com>
2026-01-30 10:39:32 +0530

    remove some bugs

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -10,19 +10,10 @@ import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// CURRENT USER
 router.get("/profile", protect, getUserProfile);
-
-// ✅ GET user by ID
 router.get("/:id", protect, getUserById);
-
-// ✅ UPDATE user by ID
 router.put("/:id", protect, updateUser);
-router.post("/register",registerUser);
-router.post("/login",loginUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
-
-
-
-
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import userRoutes from "./routes/userRoutes.js";
+import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/postRoutes.js";
 
 
