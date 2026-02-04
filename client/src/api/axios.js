fad0800a8fea7eb98befff5f1c4e0e6a4ca344ee
sourabh575 <139305542+sourabh575@users.noreply.github.com>
2026-02-04 12:02:33 +0530

    change backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { logoutToLogin } from "../utils/auth";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api", // backend base URL
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
 });
 
 // Add token to requests automatically
