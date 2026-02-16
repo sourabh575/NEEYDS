3bc9c25827cc97fd3792c057ca2805a5bc6386f2
sourabh575 <139305542+sourabh575@users.noreply.github.com>
2026-02-17 02:57:22 +0530

    fix oAuth issues

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.YOUR_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </StrictMode>
